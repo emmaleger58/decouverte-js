4f537fe47bb2d5f0dd13d07e51153b2ce5658212
Emma Leger <e.leger@codeur.online>
2020-06-17 16:55:20 +0200

    try to stop #pion so it doesn't leave #plateau

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var plateau = document.querySelector('#plateau'); // variables objet
-var pion = document.querySelector('#pion');
-var x = 0;
+//var plateau = document.getElementById('plateau'); // variables objet
+var pion = document.getElementById('pion');
+var x;
 var y;
 
 // programmation des touches de direction
@@ -15,33 +15,32 @@ document.onkeydown = function(event) {
 function gauche() {
   x = getComputedStyle(pion).left;
   x = parseInt(x);
-  x = x - 20;
-  x = x + "px";
+  x = x - 40 + "px";
+  pion.style.left = x;
+}
+
+function droite() {
+  x = getComputedStyle(pion).left;
+  x = parseInt(x);
+  x = x + 40 + "px";
   pion.style.left = x;
 }
 
 function haut() {
   y = getComputedStyle(pion).top;
   y = parseInt(y);
-  y = y - 20;
-  y = y + "px";
+  y = y - 40 + "px";
   pion.style.top = y;
 }
 
 function bas() {
   y = getComputedStyle(pion).top;
   y = parseInt(y);
-  y = y + 20;
-  y = y + "px";
+  y = y + 40 + "px";
   pion.style.top = y;
 }
 
-if (x < 800"px") {
-	function droite() {
-	  x = getComputedStyle(pion).left;
-	  x = parseInt(x);
-	  x = x + 20;
-	  x = x + "px";
-	  pion.style.left = x;
-	}
-}
+if (x < 0 + "px") x = 0 + "px"; // Si elles sont inférieures à 0
+if (y < 0 + "px") y = 0 + "px";
+if (x > 760 + "px") x = 760 + "px";// Si elles sont supérieures à 760
+if (y > 760 + "px") y = 760 + "px";
